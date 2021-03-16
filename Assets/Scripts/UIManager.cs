27743933bad89637a48eddfed62fae002703a352
dnsdnz <berfz23@gmail.com>
2021-03-16 10:43:29 +0300

    UIManager script created for controlling UI elements and objects added

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [Header("Top Bar")]
+    public Text scoreText;
+    public Text highScoreText;
+    public Text movementText;
+   
+
+}
